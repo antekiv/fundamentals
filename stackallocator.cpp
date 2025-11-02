e3cf15d2870919e13d4f6d809887c00cdea2d3d1
Anton <antekiv@github.com>
2025-11-02 10:16:07 +0300

    list: test 1 passed!

--- stackallocator.cpp
+++ stackallocator.cpp
@@ -25,14 +25,13 @@ class List {
 private:
     template <bool IsConst>
     class base_iterator {
-
     public:
-    
+        using difference_type = std::ptrdiff_t;
+        using iterator_category = std::bidirectional_iterator_tag;
         using pointer_type = std::conditional_t<IsConst, const T*, T*>;
         using reference_type = std::conditional_t<IsConst, const T&, T&>;
-        using difference_type = std::ptrdiff_t;
-        using value_type = T;
-        using iterator_category = std::bidirectional_iterator_tag;
+        using value_type = std::conditional_t<IsConst, const T, T>;
+
     private:
         friend class List<T, Allocator>;
         
@@ -42,7 +41,7 @@ private:
         base_node_pointer ptr_;
 
         base_iterator(base_node_pointer ptr): ptr_(ptr) {}
-    public:
+    public:        
         base_iterator(const base_iterator&) = default;
 
         base_iterator& operator=(const base_iterator& other) = default;
@@ -51,8 +50,8 @@ private:
             return ptr_ == other.ptr_;
         }
         
-        reference_type operator*() const {return static_cast<Node*>(ptr_)->value;};
-        pointer_type operator->() const {return &(static_cast<Node*>(ptr_)->value);}
+        reference_type operator*() const {return static_cast<node_pointer>(ptr_)->value;};
+        pointer_type operator->() const {return &(static_cast<node_pointer>(ptr_)->value);}
 
         base_iterator& operator++() {
             ptr_ = ptr_->next;
@@ -123,27 +122,27 @@ public:
 
     // reverse_iterators
     reverse_iterator rbegin() {
-        return reverse_iterator{end()};
+        return reverse_iterator{&fake_node_};
     }
 
     reverse_iterator rend() {
-        return reverse_iterator{begin()};
+        return reverse_iterator{fake_node_.next};
     }
 
     const_reverse_iterator rbegin() const {
-        return const_reverse_iterator{end()};
+        return const_reverse_iterator{&fake_node_};
     }
 
     const_reverse_iterator rend() const {
-        return const_reverse_iterator{begin()};
+        return const_reverse_iterator{fake_node_.next};
     }
 
-    const_reverse_iterator crbegin() {
-        return const_reverse_iterator{cend()};
+    const_reverse_iterator crbegin() const {
+        return const_reverse_iterator{&fake_node_};
     }
 
-    const_reverse_iterator crend() {
-        return const_reverse_iterator{cbegin()};
+    const_reverse_iterator crend() const {
+        return const_reverse_iterator{fake_node_.next};
     }
 
 public:
@@ -154,22 +153,15 @@ public:
         , alloc_(alloc) {}
 
     List(const List& other) {
-        
-        std::cout << other.size() << std::endl;
-        //List temp(other.alloc_);
-        std::cout << "4\n";
-        for (auto el : other) {
-          //  temp.push_back(el);
+        // TODO: check yourself
+
+        this->fake_node_ = {&this->fake_node_, &this->fake_node_};
+        this->sz_ = 0;
+        this->alloc_ = other.alloc_;
+
+        for (const auto& el : other) {
+            this->push_back(el);
         }
-
-        std::cout << "LI\n";
-
-        /*
-        std::swap(this->fake_node_, temp.fake_node_);
-        std::swap(this->sz_, temp.sz_);
-        this->alloc_ = temp.alloc_;
-        std::cout << "LISTTT\n";
-        */
     }
 
     ~List() {
@@ -185,7 +177,10 @@ public:
     template <typename... Args>
     [[maybe_unused]] iterator insert(const_iterator pos, Args&&... args) {
         Node* new_node = create_node(std::forward<Args>(args)...);
-        rebind_nodes(pos, new_node);
+        
+        BaseNode* current = const_cast<BaseNode*>(pos.ptr_);
+
+        rebind_nodes(current, new_node);
         ++sz_;
         return iterator {new_node};
     }
@@ -199,11 +194,13 @@ public:
     }
 
     [[maybe_unused]] iterator erase(const_iterator pos) {
-        const_iterator prev = pos - 1;
-        pos = delete_node(pos);
-        bind_nodes(prev, pos);
+        Node* curr = static_cast<Node*>(const_cast<BaseNode*>(pos.ptr_));
+        Node* prev = static_cast<Node*>(curr->prev);
 
-        return iterator(pos.ptr_);
+        auto next = delete_node(curr);
+        bind_nodes(prev, next);
+
+        return iterator(next);
     }
 
     void pop_front() {
@@ -231,27 +228,27 @@ private:
         return new_arr;
     }
 
-    const_iterator delete_node(const_iterator pos) {
-        const_iterator ret = pos + 1;
+    Node* delete_node(Node* node) {
+        Node* ret = static_cast<Node*>(node->next);
         
-        AllocTraits::destroy(alloc_, &(static_cast<Node*>(pos.ptr_)->value));
-        AllocTraits::deallocate(alloc_, static_cast<Node*>(pos.ptr_), 1);
+        AllocTraits::destroy(alloc_, &(node->value));
+        AllocTraits::deallocate(alloc_, node, 1);
         --sz_;
 
         return ret;
     }
 
-    void bind_nodes(const_iterator lhs, const_iterator rhs) {
-        lhs.ptr_->next = rhs.ptr_;
-        rhs.ptr_->prev = lhs.ptr_;
+    void bind_nodes(BaseNode* lhs, BaseNode* rhs) {
+        lhs->next = rhs;
+        rhs->prev = lhs;
     }
 
-    void rebind_nodes(const_iterator pos, BaseNode* new_node) {        
-        BaseNode* prev_node = pos.ptr_->prev;
+    void rebind_nodes(BaseNode* pos, BaseNode* new_node) {        
+        BaseNode* prev_node = pos->prev;
 
         prev_node->next = new_node;
         new_node->prev = prev_node;
-        new_node->next = pos.ptr_;
-        pos.ptr_->prev = new_node;
+        new_node->next = pos;
+        pos->prev = new_node;
     }
 };
