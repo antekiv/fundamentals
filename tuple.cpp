8e87d5ab5f07c81f9600cec17f46e35ef283879d
Anton <antekiv@github.com>
2026-01-04 21:43:22 +0300

    tuple: init commit

--- tuple.cpp
+++ tuple.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cassert>
 #include <type_traits>
+#include <vector>
 
 // structured bindings c++17
 // auto [x, _, _] = ...
@@ -16,28 +17,42 @@ struct tuple : tuple<Tail...>{
 */
 
 template <typename... Types>
-struct tuple;
+struct Tuple;
 
 template <typename Head, typename... Tail>
-class tuple<Head, Tail...> {
-public:
+class Tuple<Head, Tail...> {
     Head head_;
-    [[no_unique_address]] tuple<Tail...> tail_;
+    [[no_unique_address]] Tuple<Tail...> tail_;
 
-public:
-    tuple(Head&& head, Tail&&... tail)
-        : head_(head), tail_(std::forward<Tail>(tail)...) {}
-
+    // and 3 overloads :thinking:
     template <size_t N, typename... Types>
-    friend decltype(auto) get(tuple<Types...>&);
+    friend decltype(auto) get(Tuple<Types...>&);
+public:
+    Tuple(const Head& head, const Tail&... tail)
+        : head_(head)
+        , tail_(tail...) 
+    {}
+    template <typename UHead, typename... UTail>
+    requires (
+        sizeof...(UTail) == sizeof...(Tail) &&
+        std::is_constructible_v<Head, UHead> &&
+        (std::is_constructible_v<Tail, UTail> && ...)
+    )
+    explicit( 
+        ! (std::is_convertible_v<UHead, Head> && 
+          (std::is_convertible_v<UTail, Tail> && ...))
+    )
+    Tuple(UHead&& head, UTail&&... tail)
+        : head_(std::forward<UHead>(head))
+        , tail_(std::forward<UTail>(tail)...) 
+    {}
 };
 
 template <>
-struct tuple<> {};
-
+struct Tuple<> {};
 
 template <size_t N, typename... Types>
-decltype(auto) get(tuple<Types...>& t) {
+decltype(auto) get(Tuple<Types...>& t) {
     if constexpr (N == 0) {
         return t.head_;
     } else {
@@ -49,7 +64,7 @@ template <typename... Types>
 struct tuple_size;
 
 template<class... Types >
-struct tuple_size<tuple<Types...>>
+struct tuple_size<Tuple<Types...>>
     : std::integral_constant<std::size_t, sizeof...(Types)> { };
 
 template<class T>
@@ -60,51 +75,3 @@ template <size_t I, typename Tuple>
 struct tuple_element {
     using type = decltype(get<I>(std::declval<Tuple&>()));
 };
-
-// get for rvalue, const
-
-void test_empty() {
-    tuple<> t;
-    assert(tuple_size_v<decltype(t)> == 0);
-}
-
-void test_basic_storage_and_access() {
-    tuple<int, std::string, double> t(42, "hello", 3.14);
-    assert(get<0>(t) == 42);
-    assert(get<1>(t) == "hello");
-    assert(get<2>(t) == 3.14);
-    assert(tuple_size_v<decltype(t)> == 3);
-}
-
-void structure_bindings()
-{
-    tuple<int, double> t(42, 3.14);
-    const auto& [a, b] = t;
-    std::cout << a << " -> " << b.head_;
-}
-
-
-// SFINAE (Substitution Failure Is Not An Error)
-template<typename T>
-typename std::enable_if_t<std::is_integral<T>::value, void>
-print_type(T value) {
-    std::cout << "Целое число: " << value << std::endl;
-}
-
-template<typename T>
-typename std::enable_if<std::is_floating_point<T>::value, void>::type
-print_type(T value) {
-    std::cout << "Число с плавающей точкой: " << value << std::endl;
-}
-
-
-int main()
-{
-    // test_empty();
-    // test_basic_storage_and_access();
-    // structure_bindings();
-
-    //print_type(65);
-    //print_type(65.545);
-
-}
