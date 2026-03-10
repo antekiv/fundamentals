4b13edd85193ee48e23e5ea9b93fdb58b7daffc8
Anton <antekiv@github.com>
2026-03-10 23:15:21 +0300

    thread_pool: shared_from_this, improve multitasking

--- concurrency/thread_pool.cpp
+++ concurrency/thread_pool.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include <array>
+#include <atomic>
 #include <concepts>
 #include <condition_variable>
 
+#include <functional>
+
+#include <memory>
 #include <mutex>
 #include <queue>
 #include <thread>
@@ -13,8 +17,11 @@ namespace {
 
 template <typename FTask,  std::size_t N>
 requires (N <= MAX_THREADS)
-class ThreadPool {
+class ThreadPool
+                : public std::enable_shared_from_this<ThreadPool<FTask, N>> {
+
     std::array<std::jthread, N> threads_;
+    // std::pair<FTask, Args...> 
     std::queue<FTask> tasks_;
     mutable std::mutex tasks_mutex_;
     std::condition_variable cv_;
@@ -25,12 +32,15 @@ public:
             th.request_stop();
         }
         cv_.notify_all();
-        std::cout << "~ThreadPool()" << std::endl; 
     }
     void Run() {
+        auto self = this->weak_from_this();
+
         for (auto& th : threads_) {
-            th = std::jthread([this](std::stop_token st) {
-                this->worker(st);
+            th = std::jthread([self](std::stop_token st) {
+                if (auto strong = self.lock()) {
+                    strong->worker(st);
+                }
             });
         }
     }
@@ -38,13 +48,17 @@ public:
     template <typename... Args>
     requires std::invocable<FTask, Args...>
     void AddTask(FTask func/*, Args&&... args*/) {
-        std::scoped_lock lock(tasks_mutex_);
-        tasks_.push(func);
+        {
+            std::scoped_lock lock(tasks_mutex_);
+            tasks_.push(func);
+        }
+        cv_.notify_one();
     }
 
 private:
     void worker(std::stop_token stoken) {
-        // while (is_should_work_) {
+
+        while (true) {
             FTask current_task;
             {
                 std::unique_lock lock(tasks_mutex_);
@@ -53,33 +67,34 @@ private:
                     return stoken.stop_requested() || !this->tasks_.empty(); 
                 });
 
-                if (stoken.stop_requested() && tasks_.empty()) {
+                if (stoken.stop_requested())
                     return;
-                }
 
-                current_task = tasks_.back();
+                if (this->tasks_.empty())
+                    continue;
+
+                current_task = tasks_.front();
                 tasks_.pop();
-                std::cout << tasks_.size() << std::endl;
+                //std::cout << tasks_.size() << std::endl;
             }
 
             current_task();
-            
-        //}
+        }
     }
 };
 
-
-void foo() {
-    std::cout << "My first job!" << std::endl;
-}
-
 int main() {
-    ThreadPool<void (*)(), 4> pool;
-    pool.AddTask(foo);
-    pool.AddTask(foo);
-
-    pool.Run();
-    // launch in separate thread
-    std::cout << "Thread pool!!!" << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    {
+        std::atomic<int> i = 0;
+        auto incr = [&i](){ ++i; };
+        std::shared_ptr<ThreadPool<std::function<void()>, 4>> pool = std::make_shared<ThreadPool<std::function<void()>, 4>>();
+        for (int i = 0; i < 1'000'000; ++i)
+        {
+            pool->AddTask(incr);
+        }
+        pool->Run();
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+        
+        std::cout << "result: " << i << std::endl;
+    }  
 }
